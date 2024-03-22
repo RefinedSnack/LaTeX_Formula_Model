1c976dc67f978ae061c33525997137ffc2699171
cmanyc <99767382+cmanyc@users.noreply.github.com>
2024-03-22 10:31:45 -0600

    working model

--- trainer.py
+++ trainer.py
@@ -39,7 +39,7 @@ class_Labels = ['0','1','2','3','4','5','6','7','8','9','division','dot','downar
 #data generators
 train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
 
-train_generator=train_datagen.flow_from_directory(r'./dataset_with_classes_arrows_and_equal/train',
+train_generator=train_datagen.flow_from_directory(r'./dataset_4 Bigger latex equation (arrows)/train',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
                                                  batch_size=24,
@@ -49,7 +49,7 @@ train_generator=train_datagen.flow_from_directory(r'./dataset_with_classes_arrow
 
 val_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
 
-val_generator=val_datagen.flow_from_directory(r'./dataset_with_classes_arrows_and_equal/val', # this is where you specify the path to the main data folder
+val_generator=val_datagen.flow_from_directory(r'./dataset_4 Bigger latex equation (arrows)/val', # this is where you specify the path to the main data folder
                                                  target_size=(224,224),
                                                  color_mode='rgb',
                                                  batch_size=24,
@@ -77,5 +77,6 @@ model.compile(optimizer=Adam(lr=0.00001),loss='categorical_crossentropy', metric
 
 step_size_train=train_generator.n//train_generator.batch_size
 step_size_val=val_generator.n//val_generator.batch_size
-history = model.fit_generator(generator=train_generator, steps_per_epoch=step_size_train, validation_data=val_generator, validation_steps=step_size_val, epochs=1, callbacks=callback)
+history = model.fit_generator(generator=train_generator, steps_per_epoch=step_size_train, validation_data=val_generator, validation_steps=step_size_val, epochs=10, callbacks=callback)
+
 model.save('./trained_model')
